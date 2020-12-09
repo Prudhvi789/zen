dc53a642dce1611224d2312ca22698455e52f0fd
Prudhvi789 <52847788+Prudhvi789@users.noreply.github.com>
2020-12-09 14:58:47 +0530

    Delete P7.js

--- P7.js
+++
@@ -1,7 +0,0 @@
-var expected = {foo: 5, bar: 6};
-var actual = {foo: 7, bar: 6}
-function assertObjectsEqual(actual, expected, testName){
- console.log(JSON.stringify(actual)===JSON.stringify(expected) ? true : false);
-}
-
-assertObjectsEqual(actual,expected);
